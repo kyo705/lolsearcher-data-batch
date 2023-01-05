6fb961d012d0c40088d8bddd9d9b19125d97568b
kyo705 <mission0705@naver.com>
2023-01-05 14:39:13 +0900

    [INITIAL] 프로젝트 생성

---
+++ src/test/java/com/lolsearcher/databatch/DataBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lolsearcher.databatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
